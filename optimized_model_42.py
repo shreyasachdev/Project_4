ad244a4b3b25fea49bb8746f35c56fca51296880
afto95 <eric.pacheco@mail.utoronto.ca>
2020-07-29 16:47:53 -0400

    Updated with commented code

--- optimized_model_42.py
+++ optimized_model_42.py
@@ -10,11 +10,13 @@ from tensorflow.keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 import joblib
 
+#Get all image paths from car_images folder and append those paths to a list
 image_paths = os.listdir('car_images')
 full_image_paths = []
 for image in image_paths:
     full_image_paths.append(f"car_images/{image}")
 
+#Create empty lists, one for the image arrays, and another for the car brand label
 image_arrays = []
 image_labels = []
 
@@ -22,28 +24,44 @@ for x in range(len(full_image_paths)):
     width = 240
     height = 320
     try:
+        #Loop through each full image path and load each image as grayscale using cv2
         array = cv2.imread(full_image_paths[x], cv2.IMREAD_GRAYSCALE)
+
+        #Resize each array specified width and height
         resized_array = cv2.resize(array,(height,width),interpolation=cv2.INTER_AREA)
+
+        #Convert back to rgb to add color channel back to array shape (Image still remains in grayscale)
         resized_array_rgb = cv2.cvtColor(resized_array,cv2.COLOR_GRAY2RGB)
+
+        #Append converted array to image_arrays list
         image_arrays.append(resized_array_rgb)
         print(full_image_paths[x])
+
+        #Simultaneously get the images brand from the image file name
         label_split = image_paths[x].split('_')
         image_labels.append(label_split[0])
     except Exception as e:
         print('failed')
 
+#Resize array to fullfill CNN required shape
 image_arrays = np.array(image_arrays).reshape(-1, 240, 320, 3)
 
+#Use label encoder and to_categorical to one hot encode the car brand labels
 label_encoder = LabelEncoder()
 label_encoder.fit(image_labels)
-label_encoder_filename = "label_encoders/label_encoder_42_classes.save"
-joblib.dump(label_encoder, label_encoder_filename) 
 encoded_y = label_encoder.transform(image_labels)
-
 y_categorical = to_categorical(encoded_y)
+
+#Get Number of car brand classifications (needed for dense layer output in CNN)
 classifications = len(y_categorical[0])
 
+#Save label_encoder object so that predictions can be transformed in other jupyter notebooks/python scripts
+label_encoder_filename = "label_encoders/label_encoder_42_classes.save"
+joblib.dump(label_encoder, label_encoder_filename) 
+
+#Split data set into train and test sets. Then split set again to get validation set
 X_train, X_test, y_train, y_test = train_test_split(image_arrays,y_categorical,random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(image_arrays,y_categorical,test_size=0.1,random_state=42)
 
 # dataGen = ImageDataGenerator(width_shift_range=0.1,
 #                             height_shift_range=0.1,
@@ -52,16 +70,19 @@ X_train, X_test, y_train, y_test = train_test_split(image_arrays,y_categorical,r
 #                             rotation_range=10)
 # dataGen.fit(X_train)
 
+#Define parameters for model
 noOfFilters1 = 64
 noOfFilters2 = 32
 sizeOfFilter1 = (5,5)
 sizeOfFilter2 = (3,3)
 opt = Adam(lr=0.03)
 
+#Initialize tensorboard object to be used for callbacks
 NAME = 'Optimized_Model'
 print(NAME)
 tensorboard = TensorBoard(log_dir=f'logs/{NAME}')
 
+#Initalize model, and add layers
 model = Sequential()
 
 model.add(Conv2D(noOfFilters1,sizeOfFilter1,activation='relu',input_shape=(240,320,3)))
@@ -76,9 +97,12 @@ model.add(Flatten())
 
 model.add(Dense(units=classifications,activation='softmax'))
 
+#Compile model
 model.compile(optimizer=opt,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
+
+#Fit model  
 try:
     model.fit(X_train, y_train,
                         batch_size=32,
@@ -91,4 +115,5 @@ except Exception as e:
 
 model.evaluate(X_test, y_test)
 
+#Save model
 model.save('models/optimized_model.h5')
--- practice_training_script.py
+++ practice_training_script.py
@@ -10,42 +10,60 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import TensorBoard
 import tensorflow as tf
 
+#Get all image paths from car_images folder and append those paths to a list
 image_paths = os.listdir('car_images')
 full_image_paths = []
 for image in image_paths:
     full_image_paths.append(f"car_images/{image}")
 
+#Create empty lists, one for the image arrays, and another for the car brand label
 image_arrays = []
 image_labels = []
 for x in range(len(full_image_paths)):
     width = 240
     height = 320
     try:
+        #Loop through each full image path and load each image as grayscale using cv2
         array_gray = cv2.imread(full_image_paths[x], cv2.IMREAD_GRAYSCALE)
+
+        #Resize each array specified width and height
         resized_array_gray = cv2.resize(array_gray,(height,width),interpolation=cv2.INTER_AREA)
+
+        #Convert back to rgb to add color channel back to array shape (Image still remains in grayscale)
         resize_array_rgb = cv2.cvtColor(resized_array_gray,cv2.COLOR_GRAY2RGB)
+
+        #Append converted array to image_arrays list
         image_arrays.append(resize_array_rgb)
         print(full_image_paths[x])
+
+        #Simultaneously get the images brand from the image file name
         label_split = image_paths[x].split('_')
         image_labels.append(label_split[0])
     except:
         pass
 
+#Resize array to fullfill CNN required shape
 image_arrays = np.array(image_arrays).reshape(-1, 240, 320, 3)
 
+#Use label encoder and to_categorical to one hot encode the car brand labels
 label_encoder = LabelEncoder()
 label_encoder.fit(image_labels)
 encoded_y = label_encoder.transform(image_labels)
-
 y_categorical = to_categorical(encoded_y)
+
+#Get Number of car brand classifications (needed for dense layer output in CNN)
 classifications = len(y_categorical[0])
 
+#Split data set into train and test sets. Then split set again to get validation set
 X_train, X_test, y_train, y_test = train_test_split(image_arrays,y_categorical,random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(image_arrays,y_categorical,test_size=0.1,random_state=42)
 
+#Set parameters to run models
 dense_layers = [0,1,2]
 layer_sizes = [32,64,128]
 conv_layers = [1,2,3]
 
+#Loop over different parameters to try different models
 for dense_layer in dense_layers:
     for layer_size in layer_sizes:
         for conv_layer in conv_layers:
